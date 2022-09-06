9d2c28c2822843ddcd77833d925d496e4d1f70d7
Martin Andersson <martin.andersson@me.com>
2022-09-06 10:12:21 +0200

    second API

--- API.js
+++ API.js
@@ -12,3 +12,23 @@ export async function handlerMessage(io, socket, msgApi) {
     }
     io.emit("msg", msgApi)
 }
+
+export async function emojiHandler(io, socket, msgEmoji) {
+const res = await fetch ('https://mojitok-mojitok-emoticons-v1.p.rapidapi.com/emoticons?text=%3CREQUIRED%3E&language=en', {
+    
+    method: 'GET',
+    headers: {
+        'mojitok-token': 'undefined',
+        'X-RapidAPI-Key': '4c9b96648amsh2dc6631f46e1410p14cef6jsn330d2b37413c',
+        'X-RapidAPI-Host': 'mojitok-mojitok-emoticons-v1.p.rapidapi.com'
+    }
+    //const data = await res.json()
+
+    }) 
+}
+
+/* fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
+} */
